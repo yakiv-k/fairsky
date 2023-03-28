dcf7a5d7a00516ea5b7e29b13534f9334a687e1f
yakiv-k <yakiv.y.k@gmail.com>
2023-03-28 19:44:41 -0400

    Transferred chart markup and data to separate component, Added Chart component to Weather component

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,14 +1,6 @@
 import { useState, useEffect } from "react";
-import {
-  LineChart,
-  Line,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Text,
-  ResponsiveContainer,
-} from "recharts";
+import Chart from "../Chart/Chart";
+
 import axios from "axios";
 
 import playIcon from "../../assets/icons/playicon.svg";
@@ -114,50 +106,7 @@ function Weather() {
           ></img>
         </button>
       </div>
-      <div className="weather__chart chart">
-        <ResponsiveContainer aspect={2.3}>
-          <LineChart width="50%" height={300} data={graphArr}>
-            <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis
-              dataKey="timestamp"
-              interval={0}
-              style={{
-                display: "none",
-              }}
-              label={
-                <Text
-                  className="name__x"
-                  x={-15}
-                  y={0}
-                  dx={50}
-                  dy={250}
-                  offset={0}
-                  angle={-90}
-                >
-                  Temperature
-                </Text>
-              }
-            />
-            <YAxis
-              label={
-                <Text
-                  className="name__y"
-                  x={0}
-                  y={420}
-                  dx={475}
-                  dy={0}
-                  offset={0}
-                  angle={0}
-                >
-                  Timestamp
-                </Text>
-              }
-            />
-            <Tooltip />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
+      <Chart chartData={graphArr}/>
     </section>
   );
 }
